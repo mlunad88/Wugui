a32a2bfdfacfdc61cf017f5e04f25f9615818890
Marcelo <m.lunad@alumnos.upm.es>
2023-05-17 14:46:29 +0200

    implementado codigo Rocky y Tank

--- Sapoconchos/CODIGO/Marce_Sapoconchos/Marce_Sapoconchos.ino
+++ Sapoconchos/CODIGO/Marce_Sapoconchos/Marce_Sapoconchos.ino
@@ -11,8 +11,28 @@
 Servo servo1;
 realimentacion realimentacion;
 
+int zero_position = 135; // Posición del servo correspondiente a 0 grados
+int servo_range = 125;  // Rango del servo entre 2, (puede ser 250 o 180 grados)
+double k= (180.0/270.0); //para los servos de 270 grados, en el de 180 k=1 (osea que no hay xd)
+
+int realiment1 = 0; //realimentacion del servo1
+
 void manual(void);
 void real(void);
+// Función para convertir ángulos negativos en grados positivos en relación a la posición de 0 grados del servo
+int convert_angle(int angle) {
+  return angle + zero_position;
+}
+// Función para recortar los ángulos de giro al rango del servo
+int clip(int angle) {
+  if (angle < -servo_range + zero_position) {
+    return -servo_range + zero_position;
+  } else if (angle > servo_range + zero_position) {
+    return servo_range + zero_position;
+  } else {
+    return angle;
+  }
+}
 
 void setup() {
   pinMode(P1,INPUT);
@@ -22,11 +42,15 @@ void setup() {
 
   // Unimos los servos al pin que les corresponde
   servo1.attach(S1);
+  myservo.write(zero_position); // Establecer la posición inicial del servo
 }
 
 void loop() {
+  int angle = 0;  // Ejemplo de ángulo
+  angle = convert_angle(angle); // Convertir ángulo a grados positivos en relación a la posición de 0 grados
+  angle = clip(angle); // Obtener ángulo recortado al rango del servo
   realimentacion.lectura_pos = analogRead(P1);
-  realimentacion.pos_env = 45;
+  realimentacion.pos_env = k*angle;
   real();
 }
 
