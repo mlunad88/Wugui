6a7b102828353ed5da321f62e7069f717c832d15
Raquel Roca Pereira <61198543+r-roca@users.noreply.github.com>
2023-05-20 17:33:58 +0200

    Add files via upload

---
+++ Sapoconchos/CODIGO/ROCKY_ULTIMA REU DISTINTOS CODIGOS/MI_CODGIO_FINAL/CInversa.cpp.ino
@@ -0,0 +1,58 @@
+#include "CInversa.h"
+#include <math.h>
+
+CInversa::CInversa(float xi,float yi, float zi){
+  x=0.0;
+  y=0.0;
+  z=0.0;
+  l1 = 100.0;
+  l2 = 150.0;
+  l3 = 83.0;
+  l4 = 67.0;
+  ang1=0.0;
+  ang2=0.0;
+  ang3=0.0;
+  ang4=0.0;
+}
+
+void calcular_angulos(float xi, float yi, float zi){
+  x=xi;
+  y=yi;
+  z=zi;
+  
+  calcular_ang1();
+  calcular_ang2();
+  calcular_ang3();
+  calcular_ang4();
+  calcular_theta();
+}
+
+void CInversa::calcular_theta(){
+  theta=ang2+ang3+ang4;
+}
+
+void CInversa::calcular_ang1(){
+  double a1;
+  a1= atan(x,y);
+  ang1=a1;
+}
+
+void CInversa::calcular_ang3(){
+  double a3;
+  a3=atan(/*+-*/(sqrt(1-cos(ang3)*cos(ang3))/cos(ang3));
+  ang3=a3;
+}
+
+void CInversa::calcular_ang2(){
+  double a2, beta, alfa;
+  beta = atan((l2 + (l3 * cos(ang3) * cos(ang3))) / sqrt(pow((l3 * sin(ang3)), 2) + pow((l2 + (l3 * cos(ang3))), 2) - pow((z - l1 - (l4 * sin(theta))), 2))); //mas o menos en el denominador
+  alfa=atan((l3*sin(ang3))/(l2+(l3*cos(ang3))));
+  a2=beta-alfa;
+  ang2=a2;
+}
+
+void CInversa::calcular_ang4(){
+  double a4;
+  a4=theta-ang3-ang2;
+  ang4=a4;
+}
